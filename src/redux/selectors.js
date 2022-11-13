b6c1d9c33b94d9a6976c99c33638612426b34f9e
Lavrov Andrey <Sattarlav@gmail.com>
2022-11-13 13:24:35 +0200

    add })

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,13 +1,15 @@
+import { createSelector } from "@reduxjs/toolkit";
+
+
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
-// export const selectIsLoadingOnButton = state => state.contacts.isLoadingOnButton;
 export const selectError = state => state.contacts.error;
 
 export const selectFilter = state => state.filter;
 
-export const selectFilterByName = (state) => {
-    const filter = selectFilter(state);
-    const contacts = selectContacts(state);
+export const selectFilterByName = createSelector([selectContacts, selectFilter],
+    (contacts, filter) => {
+    console.log('selectFilterByName')
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
-};
+})
